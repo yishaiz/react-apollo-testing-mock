fe9b09e68d5d677719e6e4bc0808144c8cf0b155
Yishai Zaltzberg <Yishai.Zaltzberg@samanage.com>
2020-10-20 11:29:21 +0300

    04-query-promise-resolve-faile

--- src/components/Dog.test.js
+++ src/components/Dog.test.js
@@ -77,3 +77,29 @@ it('should render loading state initially', () => {
     // console.log({component, tree})
 });
 
+it('should render dog (after loading)', async () => {
+    const dogMock = {
+        request: {
+            query: GET_DOG_QUERY,
+            variables: { name: 'Buck' },
+        },
+        result: {
+            data: { dog: { id: 1, name: 'Buck', breed: 'poodle' } },
+        },
+    };
+
+    const component = renderer.create(
+        <MockedProvider mocks={[dogMock]} addTypename={false}>
+            <Dog name="Buck" />
+        </MockedProvider>,
+    );
+
+    await new Promise(resolve => setTimeout(resolve, 100)); // wait for response
+
+    const p = component.root.findByType('p');
+    expect(p.children).toContain('Buck is a poodle');
+});
+
+
+
+
